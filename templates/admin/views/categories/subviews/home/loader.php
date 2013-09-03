aad1b8df271e2d43511630bb6a72e87822f175f0
Rellynn <rellynn@icloud.com>
2013-09-03 18:13:40 +0200

    Correction du non-affichage de la dernière catégorie créée

--- templates/admin/views/categories/subviews/home/loader.php
+++ templates/admin/views/categories/subviews/home/loader.php
@@ -3,5 +3,5 @@ if(!isset($params[2])) {
 	$params[2] = 1;
 }
 $categories = $_categories->getCategories(10, $params[2]);
-array_pop($categories);
+array_shift($categories);
 $count_pages = ((int)$_categories->countAll() / 10) + 1;
